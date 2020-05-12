0ccc40aacac0bb28340c0eb5f2602a6f761ecc1b
echua3 <epiphany.chua3@gmail.com>
2020-05-12 00:59:38 -0700

    Instantiate Many Particles

--- AR Magnetic Fields/Assets/Scripts/InstantiationExample.cs
+++ AR Magnetic Fields/Assets/Scripts/InstantiationExample.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Creates points based on the location of the magnet
 public class InstantiationExample : MonoBehaviour
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
+    private List<PermanentMagnet> permanentMagnets;
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
+        permanentMagnets = new List<PermanentMagnet>(FindObjectsOfType<PermanentMagnet>());
         // Instantiate at position (0, 0, 0) and zero rotation.
-        Instantiate(myPrefab, new Vector3(0, 1, 1), Quaternion.identity);
+        foreach (PermanentMagnet pm in permanentMagnets)
+        {
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y + 1, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y + 2, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y + 3, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y - 1, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y - 2, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y - 3, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y - 1, pm.transform.position.z + 3), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 1, pm.transform.position.y - 1, pm.transform.position.z - 3), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 4, pm.transform.position.y - 1, pm.transform.position.z + 1), Quaternion.identity);
+            Instantiate(myPrefab, new Vector3(pm.transform.position.x + 4, pm.transform.position.y - 1, pm.transform.position.z + 1), Quaternion.identity);
+        }
+        
     }
 }
