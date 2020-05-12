74051b127fc046533b2c0ba7fc0f6f6bd49f7c4a
echua3 <epiphany.chua3@gmail.com>
2020-05-11 19:14:01 -0700

    Scripts

---
+++ AR Magnetic Fields/Assets/Scripts/ChargedParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float charge = 1;
+
+    // Remanence field in Teslas
+    public float RemaneceField = .35;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
