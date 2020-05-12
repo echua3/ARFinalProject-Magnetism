c7e53aa8a10a0f1d6e3b9ec753d5c94f391cdc04
echua3 <epiphany.chua3@gmail.com>
2020-05-11 20:27:31 -0700

    Update Particle Locations

--- AR Magnetic Fields/Assets/Scripts/ParticleLocations.cs
+++ AR Magnetic Fields/Assets/Scripts/ParticleLocations.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ParticleLocations : MonoBehaviour
@@ -14,8 +15,31 @@ public class ParticleLocations : MonoBehaviour
         permanentMagnets = new List<PermanentMagnet>(FindObjectsOfType<PermanentMagnet>());
     }
 
-    private void getMagneticDipole(MovingParticle mp)
+    // get the coordinates of the particle if the permanent magnet was at the origin
+    private static Vector3 getOriginPosition(MovingParticle mp, PermanentMagnet pm)
     {
+        Vector3 aToB = mp.transform.position - pm.transform.position;
+        Vector3 relativePosition = pm.transform.InverseTransformPoint(aToB);
 
+        return relativePosition;
+    }
+
+    //
+    private Vector3 getMagneticDipole(MovingParticle mp)
+    {
+        Vector3 newForce = Vector3.zero;
+
+        foreach (PermanentMagnet pm in permanentMagnets)
+        {
+            Vector3 position = getOriginPosition(mp, pm);
+            // calculate magnetic force
+            float x = position.x;
+            float y = position.y;
+            float z = position.z;
+            float denominator = (float)(4 * Math.PI * Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2)));
+            Vector3 multDirection = (3 * z * x, 3 * z * y, (float)(2 * Math.Pow(z, 2) - Math.Pow(x, 2) - Math.Pow(y, 2)));
+            Vector3 force = ((float)(pm.RemanenceField)/denominator) * multDirection;
+            
+        }
     }
 }
